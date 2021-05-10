ae149d2518369bcdc0cc6754483391113d1dc9fa
Douglas1303 <douglassantos6@outlook.com>
2021-05-10 17:04:47 -0300

    Conectando ao MongoDb

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); 
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb+srv://ApiNode:244005@apinode.lv4wl.mongodb.net/test', { useNewUrlParser:true, useUnifiedTopology: true}); 
 
 //Carregar as rotas
 const indexRoute = require('./routes/index-route'); 
